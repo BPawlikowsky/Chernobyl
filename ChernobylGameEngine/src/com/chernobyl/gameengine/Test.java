85f00e67ffbc92a1d3fda763066f399ad33cfec4
B. Pawlikowsky <bartospawlos@gmail.com>
2022-02-06 13:51:00 +0100

    Project setup, video #4

---
+++ ChernobylGameEngine/src/com/chernobyl/gameengine/Test.java
@@ -0,0 +1,7 @@
+package com.chernobyl.gameengine;
+
+public class Test {
+    public static void print() {
+        System.out.println("Welcome to the Chernobyl Engine!");
+    }
+}
---
+++ src/com/chernobyl/client/App.java
@@ -0,0 +1,10 @@
+package com.chernobyl.client;
+
+import com.chernobyl.gameengine.Test;
+
+public class App {
+
+    public static void main(String[] args) {
+        Test.print();
+    }
+}
